555214877c63d15c4a2839543af962655f0bd9f6
morochrislegal <christian.moro@escola.pr.gov.br>
2026-05-22 11:09:26 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        String nome = "Christian";
+        int idade = 16;
+        double altura = 1.78;
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
---
+++ programa.bat
@@ -0,0 +1,4 @@
+@echo off 
+echo Executando o programa java.....
+java -cp "C:\Users\Aluno\Downloads\aula_java-main" Programa 
+pause
